39bab1e1de199559b631e6a0fa343439953074ef
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-10-30 20:30:17 -0500

    move

---
+++ Project 2/dmv.cpp
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t callNumber
+
+const int MAX_CUSTOMER = 20;
+const int AGENTS = 2;
+
+void *CustomerThread(void *arg)
+{
+
+}
+
+void *InfodeskThread(void *arg)
+{
+
+}
+
+void *AnnouncerThread(void *arg)
+{
+
+}
+
+void *AgentThread(void *arg)
+{
+
+}
+
+int main()
+{
+    
+}
